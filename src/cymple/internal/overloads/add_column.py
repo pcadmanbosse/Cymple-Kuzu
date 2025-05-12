a43cdddc3aa69b05c80387d7f4a078b82303a0ad
pcadmanbosse <pierre.cadman@outlook.com>
2025-05-12 15:15:53 +0000

    Fixed issue with linter formatting single quote f-string on several lines

--- src/cymple/internal/overloads/add_column.py
+++ src/cymple/internal/overloads/add_column.py
@@ -1,3 +1,4 @@
 def add_column(name: str, type: str, primary_key: bool = False, default_value: any = None, if_not_exists: bool = True):
-    query_part = f" ADD{" IF NOT EXISTS" if if_not_exists else ""} {name} {type}{f" DEFAULT {default_value}" if default_value is not None else ""}{" PRIMARY KEY" if primary_key else ""}"
+    query_part = f""" ADD{" IF NOT EXISTS" if if_not_exists else ""} {name} {type}{f" DEFAULT {default_value}" if default_value is not None else ""}{
+                     " PRIMARY KEY" if primary_key else ""}"""
     return AddColumnAvailable(self.query + query_part)
